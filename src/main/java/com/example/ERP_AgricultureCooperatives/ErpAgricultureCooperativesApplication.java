9f2d1ed4818c971db9c15d815c6622469f3d7820
MohitJ2003 <johrimohit1702@gamil.com>
2025-02-11 21:53:46 +0530

    My Agriculture Cooperatives ERP Project

---
+++ src/main/java/com/example/ERP_AgricultureCooperatives/ErpAgricultureCooperativesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ERP_AgricultureCooperatives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAgricultureCooperativesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpAgricultureCooperativesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ERP_AgricultureCooperatives/ErpAgricultureCooperativesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ERP_AgricultureCooperatives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAgricultureCooperativesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
